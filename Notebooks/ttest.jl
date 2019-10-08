75f54e85a6d9b203d6ba9ba1bd89f3023fe01f2e
Kevin Bonham <kevbonham@gmail.com>
2019-10-08 08:56:43 -0400

    fix simulation to use full distribution

--- Notebooks/ttest.jl
+++ Notebooks/ttest.jl
@@ -7,8 +7,9 @@
 #
 # This example is taken from [Data Analysis for Life Scientists](https://leanpub.com/dataanalysisforthelifesciences)
 #
-# Control mouse population: https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv
-# Experiment: https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv
+# - Control mouse population: https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv
+# - Large group experiment: https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/mice_pheno.csv
+# - 12 x 12 experiment: https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv
 
 using CSV
 using DataFrames
@@ -18,11 +19,12 @@ using StatsPlots
 using HTTP
 
 controls_url = "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+phenotypes_url = "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/mice_pheno.csv"
 hfd_url = "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
 
 controls = CSV.read(HTTP.get(controls_url).body)
+phenotypes = CSV.read(HTTP.get(phenotypes_url).body)
 hfd = CSV.read(HTTP.get(hfd_url).body)
-
 ## make a histogram of control mice
 histogram(controls.Bodyweight)
 
@@ -32,7 +34,7 @@ describe(controls.Bodyweight)
 
 # This shows what the weights of a population of normal femal mice
 # would look like, absent any special treatment.
-# Now let's look at an experiemtn with 24 mice,
+# Now let's look at an experiment with 24 mice,
 # where 12 mice were fed normal chow,
 # and the other 12 were given a high-fat diet.
 
@@ -41,7 +43,8 @@ hf = hfd[hfd.Diet .== "hf", :Bodyweight]
 chowc = RGBA(0.9,0.2,0.3, 0.5)
 hfc = RGBA(0.2,0.7,0.3, 0.5)
 
-histogram(chow, color=chowc, label="chow")
+histogram(chow, color=chowc, label="chow",
+        ylabel="count", xlabel="weight (g)")
 histogram!(hf, color=hfc, label="high fat")
 
 #-
@@ -78,5 +81,60 @@ count(x-> x > quantile(hf, .5), controls.Bodyweight) / nrow(controls)
 #
 # A t-test can be used to test the null hypothesis (h₀) that the two groups
 # are drawn from the same population.
+# Since we don't know whether or not the distributions of weight
+# in the normal chow and high fat groups
+# have the same variance,
+# I'll use the `UnequalVarianceTTest()` function from `HypothesisTests.jl`
 
-EqualVarianceTTest(hf, chow)
+tt = UnequalVarianceTTest(hf, chow)
+
+#---
+
+@show pvalue(tt)
+
+# What this p-value means is that, under the null hypothesis
+# (that there's no difference between the normal mice and the high-fat diet mice),
+# and given the distribution in the control mice,
+# we'd expect to get the distribution we actually see about 5.3% of the time.
+#
+# Another way to show that is with a simulation.
+# Since the null hypotheses is that these mice all came from
+# the same random distribution of weights,
+# we'll get a normal distribution from the mean and standard deviation
+# of all mice,
+# randomly pull 24 samples from it a few thousand times,
+# then count how often the difference in means is the same or greater
+# than what we observed.
+
+## mean and standard deviation
+μ = mean(hfd.Bodyweight)
+σ = std(hfd.Bodyweight)
+
+## normal distribution with these parameters
+using Distributions
+D = Normal(μ, σ)
+
+#-
+
+## determine the difference between high fat and control mice
+## in our experiment
+dif = abs(mean(chow) - mean(hf))
+
+sim_controls = rand(D, 5000, 12)
+sim_hf = rand(D, 5000, 12)
+
+control_means = mean(sim_controls, dims=2)
+hf_means = mean(sim_hf, dims=2)
+
+sim_difs = abs.(control_means .- hf_means)
+
+## count how many of our simulation runs had an absolute difference
+## at least as high as our observerd difference.
+mean(x-> x >= dif, sim_difs)
+
+#-
+
+
+# ## Using Soss.jl
+#
+# test
